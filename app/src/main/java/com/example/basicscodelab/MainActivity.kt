1866bff2e3e20889d4dcaee308aef1ad166d0bef
Vikas Chaudhary <vikaschaudhary8286@gmail.com>
2023-01-25 23:53:20 +0530

    Used lazy column to create a lomg enough scrollable list

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -17,6 +17,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
@@ -134,17 +136,20 @@ fun GreetingsPreview() {
         Greetings()
     }
 }
-
+@Preview
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    //1000 boxes.
+    names: List<String> = List(1000){"$it"}
 ){
-    Column(modifier=modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+    // provide scrolling.
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
+
 }
 
 @Preview
